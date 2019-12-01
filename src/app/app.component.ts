d82708cdd07023b3fcabd3e73b7c0f4deccac4d0
stefan <sdecimelli97@gmail.com>
2019-12-01 15:08:49 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'untitled2';
+
+    userName: string;
+    private response: any;
+
+    constructor(private http: HttpClient) {
+    }
+
+    search() {
+        this.http.get('https://api.github.com/users/' + this.userName)
+            .subscribe((response) => {
+                this.response = response;
+                console.log(this.response);
+            });
+    }
+
 }
